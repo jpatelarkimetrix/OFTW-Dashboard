ecf20a60325b00adde61990ccba15d03c3794794
justin <jpatel@arkimetrix.com>
2025-04-08 15:58:16 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import dash
+from dash import html, Dash, dcc
+
+app = Dash(__name__, use_pages = True)
+
+app.layout = html.Div([
+    html.H1('Dashboard'),
+    html.Div([
+        html.Div(
+            dcc.Link(f"{page['name']}", href=f"{page['relative_path']}"),
+        ) for page in dash.page_registry.values()
+    ]),
+    dash.page_container
+])
+
+if __name__ == '__main__':
+    app.run(debug = True)
---
+++ pages/page2.py
@@ -0,0 +1,9 @@
+import dash
+from dash import html
+
+dash.register_page(__name__, path = '/page2')
+
+def layout(**kwargs):
+    return html.Div([
+        html.H1('This is page2'),
+    ])
